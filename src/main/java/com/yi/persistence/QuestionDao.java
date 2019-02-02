11797de3b81da86467be9d93a45b66809c52d909
mia94 <mia_3@hanmail.net>
2019-02-02 16:27:05 +0900

    mapper작성중

--- src/main/java/com/yi/persistence/QuestionDao.java
+++ src/main/java/com/yi/persistence/QuestionDao.java
@@ -1,5 +1,13 @@
 package com.yi.persistence;
 
-public interface QuestionDao {
+import java.util.List;
 
+import com.yi.domain.QuestionVO;
+
+public interface QuestionDao {
+	public List<QuestionVO> selectByAll();
+	public QuestionVO selectByNO();
+	public void insert();
+	public void delete();
+	public void update();
 }
