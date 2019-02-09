c928592645cf9fd0d3c6249e97b407c3ea71d184
mia94 <mia_3@hanmail.net>
2019-02-09 21:29:05 +0900

    ddl 수정

--- Script/ddl.sql
+++ Script/ddl.sql
@@ -15,8 +15,7 @@ CREATE TABLE `questionBank`.`question` (
 	`correct`        INT(1)       NOT NULL COMMENT '정답', -- 정답
 	`state`          VARCHAR(20)  NOT NULL COMMENT '오류/수정예정', -- 문제상태
 	`correct_rate`   INT          NULL     COMMENT '정답률', -- 정답률
-	`picture`        TEXT         NULL     COMMENT '그림/보기', -- 그림/보기
-	`subject`        CHAR(1)      NOT NULL COMMENT '과목코드' -- 과목코드
+	`picture`        TEXT         NULL     COMMENT '그림/보기' -- 그림/보기
 )
 COMMENT '문제(1)';
 
@@ -29,9 +28,10 @@ ALTER TABLE `questionBank`.`question`
 
 -- 과목(5과목 20문제)
 CREATE TABLE `questionBank`.`subject` (
-	`subject_code` CHAR(1)     NOT NULL COMMENT '과목코드', -- 과목코드
-	`subject_name` VARCHAR(20) NOT NULL COMMENT '과목이름', -- 과목이름
-	`test`         CHAR(4)     NOT NULL COMMENT '시험코드' -- 시험코드
+	`question_code` CHAR(10)    NOT NULL COMMENT '문제코드', -- 문제코드
+	`test`          CHAR(4)     NOT NULL COMMENT '시험코드', -- 시험코드
+	`subject_code`  CHAR(1)     NOT NULL COMMENT '과목', -- 과목
+	`subject_name`  VARCHAR(20) NOT NULL COMMENT '과목이름' -- 과목이름
 )
 COMMENT '과목(5과목 20문제)';
 
@@ -39,7 +39,8 @@ COMMENT '과목(5과목 20문제)';
 ALTER TABLE `questionBank`.`subject`
 	ADD CONSTRAINT `PK_subject` -- 과목(5과목 20문제) 기본키
 		PRIMARY KEY (
-			`subject_code` -- 과목코드
+			`question_code`, -- 문제코드
+			`test`           -- 시험코드
 		);
 
 -- 출제시험(100문제)
@@ -76,42 +77,49 @@ ALTER TABLE `questionBank`.`customer`
 			`customer_code` -- 회원코드
 		);
 
--- 답
-CREATE TABLE `questionBank`.`solution` (
-	`solution_code` INT      NOT NULL COMMENT '답 코드', -- 답 코드
-	`question`      CHAR(10) NOT NULL COMMENT '문제코드', -- 문제코드
-	`answer`        INT(1)   NULL     COMMENT '입력한 답', -- 입력한 답
-	`correct`       TINYINT  NULL     COMMENT '정답여부', -- 정답여부
-	`customer`      CHAR(4)  NOT NULL COMMENT '회원코드' -- 회원코드
+-- 과목별(20문제) 응시시험
+CREATE TABLE `questionBank`.`unitTest` (
+	`unitTest_code` INT      NOT NULL COMMENT '과목별시험 코드', -- 과목별시험 코드
+	`answer`        INT(1)   NULL     COMMENT '입력한 답(20개)', -- 입력한 답(20개)
+	`correct`       TINYINT  NULL     COMMENT '정답(20개)', -- 정답(20개)
+	`customer`      CHAR(4)  NOT NULL COMMENT '회원코드', -- 회원코드
+	`question_code` CHAR(10) NULL     COMMENT '문제코드', -- 문제코드
+	`test`          CHAR(4)  NULL     COMMENT '시험코드' -- 시험코드
 )
-COMMENT '답';
+COMMENT '과목별(20문제) 응시시험';
 
--- 답
-ALTER TABLE `questionBank`.`solution`
-	ADD CONSTRAINT `PK_solution` -- 답 기본키
+-- 과목별(20문제) 응시시험
+ALTER TABLE `questionBank`.`unitTest`
+	ADD CONSTRAINT `PK_unitTest` -- 과목별(20문제) 응시시험 기본키
 		PRIMARY KEY (
-			`solution_code` -- 답 코드
+			`unitTest_code` -- 과목별시험 코드
 		);
 
-ALTER TABLE `questionBank`.`solution`
-	MODIFY COLUMN `solution_code` INT NOT NULL AUTO_INCREMENT COMMENT '답 코드';
+ALTER TABLE `questionBank`.`unitTest`
+	MODIFY COLUMN `unitTest_code` INT NOT NULL AUTO_INCREMENT COMMENT '과목별시험 코드';
 
--- 응시시험
+-- 시험 결과
 CREATE TABLE `questionBank`.`resultTest` (
-	`resultTest_code` CHAR(4) NOT NULL COMMENT '새 컬럼', -- 새 컬럼
-	`customer`        CHAR(4) NOT NULL COMMENT '회원코드', -- 회원코드
+	`resultTest_code` INT     NOT NULL COMMENT '결과시험코드', -- 결과시험코드
+	`customer_code`   CHAR(4) NOT NULL COMMENT '회원코드', -- 회원코드
+	`answer`          INT     NULL     COMMENT '입력한 답(100)', -- 입력한 답(100)
+	`correct`         INT     NULL     COMMENT '답(100)', -- 답(100)
+	`spendTime`       INT     NULL     COMMENT '걸린 시간', -- 걸린 시간
 	`score`           INT     NOT NULL COMMENT '시험성적', -- 시험성적
 	`pass`            TINYINT NOT NULL COMMENT '합격여부(60점)', -- 합격여부(60점)
 	`test`            CHAR(4) NOT NULL COMMENT '시험코드' -- 시험코드
 )
-COMMENT '응시시험';
+COMMENT '시험 결과';
 
--- 응시시험
+-- 시험 결과
 ALTER TABLE `questionBank`.`resultTest`
-	ADD CONSTRAINT `PK_resultTest` -- 응시시험 기본키
+	ADD CONSTRAINT `PK_resultTest` -- 시험 결과 기본키
 		PRIMARY KEY (
-			`resultTest_code` -- 새 컬럼
+			`resultTest_code` -- 결과시험코드
 		);
+
+ALTER TABLE `questionBank`.`resultTest`
+	MODIFY COLUMN `resultTest_code` INT NOT NULL AUTO_INCREMENT COMMENT '결과시험코드';
 
 -- 건의사항
 CREATE TABLE `questionBank`.`board` (
@@ -137,7 +145,7 @@ ALTER TABLE `questionBank`.`board`
 
 -- 건의사항 댓글
 CREATE TABLE `questionBank`.`reply` (
-	`reply_code` CHAR(4)       NOT NULL COMMENT '댓글코드', -- 댓글코드
+	`reply_code` INT           NOT NULL COMMENT '댓글코드', -- 댓글코드
 	`board`      INT           NOT NULL COMMENT '게시글코드', -- 게시글코드
 	`replytext`  VARCHAR(1000) NOT NULL COMMENT '댓글내용', -- 댓글내용
 	`regdate`    TIMESTAMP     NOT NULL DEFAULT now() COMMENT '입력날짜', -- 입력날짜
@@ -153,15 +161,8 @@ ALTER TABLE `questionBank`.`reply`
 			`reply_code` -- 댓글코드
 		);
 
--- 문제(1)
-ALTER TABLE `questionBank`.`question`
-	ADD CONSTRAINT `FK_subject_TO_question` -- 과목(5과목 20문제) -> 문제(1)
-		FOREIGN KEY (
-			`subject` -- 과목코드
-		)
-		REFERENCES `questionBank`.`subject` ( -- 과목(5과목 20문제)
-			`subject_code` -- 과목코드
-		);
+ALTER TABLE `questionBank`.`reply`
+	MODIFY COLUMN `reply_code` INT NOT NULL AUTO_INCREMENT COMMENT '댓글코드';
 
 -- 과목(5과목 20문제)
 ALTER TABLE `questionBank`.`subject`
@@ -173,19 +174,19 @@ ALTER TABLE `questionBank`.`subject`
 			`test_code` -- 시험코드
 		);
 
--- 답
-ALTER TABLE `questionBank`.`solution`
-	ADD CONSTRAINT `FK_question_TO_solution` -- 문제(1) -> 답
+-- 과목(5과목 20문제)
+ALTER TABLE `questionBank`.`subject`
+	ADD CONSTRAINT `FK_question_TO_subject` -- 문제(1) -> 과목(5과목 20문제)
 		FOREIGN KEY (
-			`question` -- 문제코드
+			`question_code` -- 문제코드
 		)
 		REFERENCES `questionBank`.`question` ( -- 문제(1)
 			`question_code` -- 문제코드
 		);
 
--- 답
-ALTER TABLE `questionBank`.`solution`
-	ADD CONSTRAINT `FK_customer_TO_solution` -- 회원 -> 답
+-- 과목별(20문제) 응시시험
+ALTER TABLE `questionBank`.`unitTest`
+	ADD CONSTRAINT `FK_customer_TO_unitTest` -- 회원 -> 과목별(20문제) 응시시험
 		FOREIGN KEY (
 			`customer` -- 회원코드
 		)
@@ -193,19 +194,31 @@ ALTER TABLE `questionBank`.`solution`
 			`customer_code` -- 회원코드
 		);
 
--- 응시시험
-ALTER TABLE `questionBank`.`resultTest`
-	ADD CONSTRAINT `FK_customer_TO_resultTest` -- 회원 -> 응시시험
+-- 과목별(20문제) 응시시험
+ALTER TABLE `questionBank`.`unitTest`
+	ADD CONSTRAINT `FK_subject_TO_unitTest` -- 과목(5과목 20문제) -> 과목별(20문제) 응시시험
 		FOREIGN KEY (
-			`customer` -- 회원코드
+			`question_code`, -- 문제코드
+			`test`           -- 시험코드
+		)
+		REFERENCES `questionBank`.`subject` ( -- 과목(5과목 20문제)
+			`question_code`, -- 문제코드
+			`test`           -- 시험코드
+		);
+
+-- 시험 결과
+ALTER TABLE `questionBank`.`resultTest`
+	ADD CONSTRAINT `FK_customer_TO_resultTest` -- 회원 -> 시험 결과
+		FOREIGN KEY (
+			`customer_code` -- 회원코드
 		)
 		REFERENCES `questionBank`.`customer` ( -- 회원
 			`customer_code` -- 회원코드
 		);
 
--- 응시시험
+-- 시험 결과
 ALTER TABLE `questionBank`.`resultTest`
-	ADD CONSTRAINT `FK_test_TO_resultTest` -- 출제시험(100문제) -> 응시시험
+	ADD CONSTRAINT `FK_test_TO_resultTest` -- 출제시험(100문제) -> 시험 결과
 		FOREIGN KEY (
 			`test` -- 시험코드
 		)
