5b74f3a44f64374c60f35b024c8b8e63791ffef6
mia94 <mia_3@hanmail.net>
2019-02-11 14:35:41 +0900

    customer컨트롤러 register, list 확인 완료

---
+++ src/main/java/com/yi/controller/CustomerController.java
@@ -0,0 +1,60 @@
+package com.yi.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.yi.domain.CustomerVO;
+import com.yi.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer/*")
+public class CustomerController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+	
+	@Autowired
+	private CustomerService service;
+	
+	@RequestMapping(value="", method=RequestMethod.POST)
+	public ResponseEntity<String> register(@RequestBody CustomerVO vo){
+		ResponseEntity<String> entity = null;
+		logger.info("CustomerVO create------------"+vo);
+		
+		try {
+			service.insertCustomer(vo);
+			entity = new ResponseEntity<String>("success", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);//400에러
+		}
+		
+		return entity;
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.GET)
+	public ResponseEntity<List<CustomerVO>>  list(){
+		ResponseEntity<List<CustomerVO>> entity = null;
+		
+		try {
+			List<CustomerVO> list = service.selectByAll();
+			entity = new ResponseEntity<>(list, HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);//List<MemberVO>로 보내야 하나, 보낼수없을때는HttpStatus만 보냄
+		}
+		
+		return entity;
+	}
+
+	
+}
