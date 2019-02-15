6d510047943bf6de5966ba05e43c9bbf49fffd1b
mia94 <mia_3@hanmail.net>
2019-02-15 17:10:10 +0900

    subjectMapper수정

--- src/main/java/com/yi/controller/SubjectController.java
+++ src/main/java/com/yi/controller/SubjectController.java
@@ -12,12 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.yi.domain.QuestionVO;
 import com.yi.domain.SubjectVO;
-import com.yi.domain.TestVO;
-import com.yi.service.QuestionService;
 import com.yi.service.SubjectService;
-import com.yi.service.TestService;
 
 @Controller
 @RequestMapping("/subject/*")
@@ -27,10 +23,6 @@ private static final Logger logger = LoggerFactory.getLogger(SubjectController.c
 	
 	@Autowired
 	private SubjectService service;
-	@Autowired
-	private TestService tService;
-	@Autowired
-	private QuestionService qService;
 	
 	@RequestMapping(value="", method=RequestMethod.POST)//
 	public ResponseEntity<String> register(@RequestBody Map<String, String> map){
@@ -44,16 +36,6 @@ private static final Logger logger = LoggerFactory.getLogger(SubjectController.c
 			SubjectVO vo = new SubjectVO();
 			vo.setSubjectCode(map.get("subjectCode"));
 			vo.setSubjectName(map.get("subjectName"));
-			
-			TestVO tVo = new TestVO();
-			tVo.setTestCode(map.get("testCode"));
-			tVo = tService.selectByNo(tVo);
-			vo.setTest(tVo);
-			
-			QuestionVO qVo = new QuestionVO();
-			qVo.setQuestionCode(map.get("questionCode"));
-			qVo = qService.selectByNO(qVo);
-			vo.setQuestion(qVo);
 			
 			service.insertSubject(vo);
 			entity = new ResponseEntity<String>("success", HttpStatus.OK);
--- src/main/java/com/yi/domain/SubjectVO.java
+++ src/main/java/com/yi/domain/SubjectVO.java
@@ -3,8 +3,6 @@ package com.yi.domain;
 public class SubjectVO {
 	private String subjectCode; 
 	private String subjectName;
-	private TestVO test; //복합키
-	private QuestionVO question;//복합키
 	 
 	public String getSubjectCode() {
 		return subjectCode;
@@ -18,26 +16,11 @@ public class SubjectVO {
 	public void setSubjectName(String subjectName) {
 		this.subjectName = subjectName;
 	}
-	public TestVO getTest() {
-		return test;
-	}
-	public void setTest(TestVO test) {
-		this.test = test;
-	}
-	public QuestionVO getQuestion() {
-		return question;
-	}
-	public void setQuestion(QuestionVO question) {
-		this.question = question;
-	}
 	
 	@Override
 	public String toString() {
-		return "SubjectVO [subjectCode=" + subjectCode + ", subjectName=" + subjectName + ", test=" + test
-				+ ", question=" + question + "]";
+		return "SubjectVO [subjectCode=" + subjectCode + ", subjectName=" + subjectName + "]";
 	}
-	
-	
 	
 	
 }
